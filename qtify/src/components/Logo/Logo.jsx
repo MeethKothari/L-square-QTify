2ddac5d97ec179b6a5fbd8dcee6ca700d6aa8b7b
Meeth <meethukothari@gmail.com>
2024-07-21 11:40:59 +0530

    updated logo allignment

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import styles from "./Logo.module.css"
 import LogoImage from "../../assets/LogoImage.png";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return <img src={LogoImage} alt="logo" width={67} className={styles.logo}/>;
 }
