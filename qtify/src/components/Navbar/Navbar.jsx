9a71ea34befa89e129b133f20f9f273681934517
Meeth <meethukothari@gmail.com>
2024-07-26 15:48:00 +0530

    logo

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 // import React from "react";
-// //import { Link } from "react-dom";
 // import { Link } from "react-router-dom";
 // import Button from "../Button/Button";
 // import Logo from "../Logo/Logo";
@@ -9,9 +8,8 @@
 // function Navbar({ searchData }) {
 //   return (
 //     <nav className={styles.navbar}>
-//       <Link to="/">
 //         <Logo />
-//       </Link>
+  
 //       <Search
 //         placeholder="Search a song of your choice"
 //         searchData={searchData}
