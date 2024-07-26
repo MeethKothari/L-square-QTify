54e3d6d657c619f34f708c6a4de8523d72a7a2bb
Meeth <meethukothari@gmail.com>
2024-07-26 17:08:30 +0530

    navbar

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import styles from "./Logo.module.css"
 import LogoImage from "../../assets/LogoImage.png";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} className={styles.logo}/>;
+  return <img src={LogoImage} alt="logo" width={67}/>;
 }
