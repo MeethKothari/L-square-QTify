f95a100b319fd11b6a0708d4d30b8f723bdc00f5
Meeth <meethukothari@gmail.com>
2024-07-26 17:11:24 +0530

    nav setting

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -4,10 +4,12 @@ import SearchIcon from '@mui/icons-material/Search';
 
 function Search({placeholder}) {
   return (
+  <>
     <div className={styles.wrapper}>
       <input className={styles.search} placeholder={placeholder}/>
       <button className={styles.searchButton}><SearchIcon/></button>
     </div>
+  </>
   )
 }
 
