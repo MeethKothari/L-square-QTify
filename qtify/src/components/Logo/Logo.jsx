cd91624aa597255002a625326cb57a20ffc3472f
Meeth <meethukothari@gmail.com>
2024-07-26 19:29:42 +0530

    logo setting

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LogoImage from "../../assets/LogoImage.png";
+import styles from "./Logo.module.css";
+
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67}/>;
+  return <img src={LogoImage} alt="logo" width={67} className={styles.logoSettings}/>;
 }
